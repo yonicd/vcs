0f5ac097dd81646e60d9ba1832ffcc068288a934
jonathans <yonicd@gmail.com>
2017-06-09 14:42:19 -0400

    update head_diff

--- R/diff_head.R
+++ R/diff_head.R
@@ -5,17 +5,24 @@
 #' @param vcs character, choose which version control system to search (git, svn), Default: 'git'
 #' @param pattern   character, string containing a regular expression, Default: NULL
 #' @param ...       arguments passed to grep
+#' @param show boolean, if false returns only boolean output of HEAD%in%local, Default: TRUE
 #' @return character
 #' @export
-diff_head=function(path='.',vcs='git',pattern=NULL,...){
+diff_head=function(path='.',vcs='git',pattern=NULL,...,show=TRUE){
   h=ls_remote(path,vcs = vcs)
-  hd=h[!h%in%list.files(path,recursive = TRUE)]
-  if(!is.null(pattern)){
-    args<-list(...)
-    args$value=TRUE
-    args$pattern=pattern
-    args$x=hd
-    hd=do.call(grep,args)
-  } 
-  hd
+  hIdx=h%in%list.files(path,recursive = TRUE)
+  if(show){
+    hd=h[!hIdx]
+    if(!is.null(pattern)){
+      args<-list(...)
+      args$value=TRUE
+      args$pattern=pattern
+      args$x=hd
+      hd=do.call(grep,args)
+    } 
+    hd
+  }else{
+    hIdx
+  }
+
 }
